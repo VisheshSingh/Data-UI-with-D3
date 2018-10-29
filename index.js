c7437a966c85a117ba497ef7b071e1de21da975c
Vishesh <visheshsingh16@gmail.com>
2018-10-29 18:56:54 -0400

    Joining data with elements on DOM

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-const canvas = d3.select(".canvas");
+const data = [{ width: 200, height: 100, fill: "purple" }];
 
-const svg = canvas
-  .append("svg")
-  .attr("height", 600)
-  .attr("width", 600);
-
-const group = svg.append("g").attr("transform", "translate(0,100)");
-
-// append shapes to svg container
-group
-  .append("rect")
-  .attr("height", 100)
-  .attr("width", 200)
-  .attr("fill", "blue")
-  .attr("x", 20)
-  .attr("y", 20);
-
-group
-  .append("circle")
-  .attr("r", 50)
-  .attr("cx", 300)
-  .attr("cy", 70)
-  .attr("fill", "pink");
-
-group
-  .append("line")
-  .attr("x1", 370)
-  .attr("x2", 400)
-  .attr("y1", 20)
-  .attr("y2", 120)
-  .attr("stroke", "red");
+const svg = d3.select("svg");
 
 svg
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 200)
-  .attr("fill", "grey")
-  .text("hello ninjas!")
-  .style("font-family", "Arial");
+  .select("rect")
+  .data(data)
+  .attr("height", function(d) {
+    return d.height;
+  })
+  .attr("width", function(d) {
+    return d.width;
+  })
+  .attr("fill", function(d) {
+    return d.fill;
+  });
