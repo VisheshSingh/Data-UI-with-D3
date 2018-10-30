e990b69b9be0b81f663936f6d817e6c576afc977
Vishesh <visheshsingh16@gmail.com>
2018-10-29 20:15:48 -0400

    Creating a linear scale

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 // select the svg container
 const svg = d3.select("svg");
 
-d3.json("planets.json").then(data => {
-  const circle = svg.selectAll("circle").data(data);
+// Create a linear scale
+const y = d3
+  .scaleLinear()
+  .domain([0, 1000])
+  .range([0, 500]);
 
-  // add attrs to circle in DOM
-  circle
-    .attr("cx", d => d.distance)
-    .attr("cy", 200)
-    .attr("r", d => d.radius)
-    .attr("fill", d => d.fill);
+console.log(y(900));
+console.log(y(0));
+console.log(y(500));
+// Get external json
+d3.json("menu.json").then(data => {
+  // build rects
+  const rects = svg.selectAll("rect").data(data);
+
+  // apply attrs to rect in DOM
+  rects
+    .attr("width", 50)
+    .attr("height", d => y(d.orders))
+    .attr("x", (d, i) => i * 70)
+    .attr("fill", "orange");
 
   // add enter selection to DOM
-  circle
+  rects
     .enter()
-    .append("circle")
-    .attr("cx", d => d.distance)
-    .attr("cy", 200)
-    .attr("r", d => d.radius)
-    .attr("fill", d => d.fill);
+    .append("rect")
+    .attr("width", 50)
+    .attr("height", d => y(d.orders))
+    .attr("x", (d, i) => i * 70)
+    .attr("fill", "orange");
 });
