0314069dcb556086acf72d255f39ad90e5c3db7e
Vishesh <visheshsingh16@gmail.com>
2018-10-29 22:22:16 -0400

    Retrieving data from firestore

--- index.js
+++ index.js
@@ -23,55 +23,61 @@ const xAxisGroup = graph
 const yAxisGroup = graph.append("g");
 
 // Get external json
-d3.json("menu.json").then(data => {
-  // Create a linear scale
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, d => d.orders)])
-    .range([graphHeight, 0]);
+db.collection("dishes")
+  .get()
+  .then(res => {
+    // console.log(res);
+    let data = [];
+    res.docs.forEach(doc => data.push(doc.data()));
+    // console.log(data);
+    // Create a linear scale
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.orders)])
+      .range([graphHeight, 0]);
 
-  // create a band scale
-  const x = d3
-    .scaleBand()
-    .domain(data.map(item => item.name))
-    .range([0, 500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+    // create a band scale
+    const x = d3
+      .scaleBand()
+      .domain(data.map(item => item.name))
+      .range([0, 500])
+      .paddingInner(0.2)
+      .paddingOuter(0.2);
 
-  // build rects
-  const rects = graph.selectAll("rect").data(data);
+    // build rects
+    const rects = graph.selectAll("rect").data(data);
 
-  // apply attrs to rect in DOM
-  rects
-    .attr("width", x.bandwidth)
-    .attr("height", d => graphHeight - y(d.orders))
-    .attr("x", d => x(d.name))
-    .attr("y", d => y(d.orders))
-    .attr("fill", "orange");
+    // apply attrs to rect in DOM
+    rects
+      .attr("width", x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.orders))
+      .attr("fill", "orange");
 
-  // add enter selection to DOM
-  rects
-    .enter()
-    .append("rect")
-    .attr("width", x.bandwidth)
-    .attr("height", d => graphHeight - y(d.orders))
-    .attr("x", d => x(d.name))
-    .attr("y", d => y(d.orders))
-    .attr("fill", "orange");
+    // add enter selection to DOM
+    rects
+      .enter()
+      .append("rect")
+      .attr("width", x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.orders))
+      .attr("fill", "orange");
 
-  //create and call the axes
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3
-    .axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d + " orders");
+    //create and call the axes
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3
+      .axisLeft(y)
+      .ticks(3)
+      .tickFormat(d => d + " orders");
 
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
 
-  xAxisGroup
-    .selectAll("text")
-    .attr("transform", "rotate(-40)")
-    .attr("text-anchor", "end")
-    .attr("fill", "orange");
-});
+    xAxisGroup
+      .selectAll("text")
+      .attr("transform", "rotate(-40)")
+      .attr("text-anchor", "end")
+      .attr("fill", "orange");
+  });
