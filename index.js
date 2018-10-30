6a269ec51f73f98f91e19e5592713d106b35a54e
Vishesh <visheshsingh16@gmail.com>
2018-10-29 20:26:50 -0400

    Create a band scale

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 // select the svg container
 const svg = d3.select("svg");
 
-// Create a linear scale
-const y = d3
-  .scaleLinear()
-  .domain([0, 1000])
-  .range([0, 500]);
-
-console.log(y(900));
-console.log(y(0));
-console.log(y(500));
 // Get external json
 d3.json("menu.json").then(data => {
+  // Create a linear scale
+  const y = d3
+    .scaleLinear()
+    .domain([0, 1000])
+    .range([0, 500]);
+
+  // create a band scale
+  const x = d3
+    .scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+
   // build rects
   const rects = svg.selectAll("rect").data(data);
 
   // apply attrs to rect in DOM
   rects
-    .attr("width", 50)
+    .attr("width", x.bandwidth)
     .attr("height", d => y(d.orders))
-    .attr("x", (d, i) => i * 70)
+    .attr("x", d => x(d.name))
     .attr("fill", "orange");
 
   // add enter selection to DOM
   rects
     .enter()
     .append("rect")
-    .attr("width", 50)
+    .attr("width", x.bandwidth)
     .attr("height", d => y(d.orders))
-    .attr("x", (d, i) => i * 70)
+    .attr("x", d => x(d.name))
     .attr("fill", "orange");
 });
